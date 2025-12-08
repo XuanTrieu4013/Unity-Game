11380dbab71ea348fb3ff20d309c8bd537a74cf3
XuanTrieu4013 <xuantrieu6123@gmail.com>
2025-12-08 15:55:29 +0700

    cap nhap them code

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,16 @@ public class PlayerController : MonoBehaviour
     
     private PlayerControls playerControls;
     private Vector2 movement;
-    private Rigidbody2D rb;
+    private Rigidbody2D rb; 
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -29,12 +33,15 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        myAnimator.SetFloat("moveX",movement.x);
+        myAnimator.SetFloat("moveY",movement.y);
     }
 
     private void Move()
@@ -42,4 +49,18 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(rb.position + movement *(moveSpeed * Time.fixedDeltaTime));
     }
 
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
 }
