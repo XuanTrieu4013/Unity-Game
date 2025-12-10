1f8f311b2085af50768840b5df5f35bb8dcca6b9
XuanTrieu4013 <xuantrieu6123@gmail.com>
2025-12-10 17:28:59 +0700

    fix sword anim

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,100 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-     public bool FacingLeft
-{
-    get { return facingLeft; }
-    set { facingLeft = value; }
-}
-
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
 
     [SerializeField] private float moveSpeed = 1f;
 
-    // Dash
-    public float dashBoost;
-    public float dashTime;
-    private float _dashTime;
-    bool isDashing = false;
-    // dash
-    
     private PlayerControls playerControls;
     private Vector2 movement;
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
     private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
+    private SpriteRenderer mySpriteRender;
+
     private bool facingLeft = false;
-    private void Awake()
-    {
+
+    private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable()
-    {
-        playerControls.Enable();       
+    private void OnEnable() {
+        playerControls.Enable();
     }
 
-    private void Update()
-    {
-        //dash
-        if (Input.GetKeyDown(KeyCode.LeftShift) && _dashTime <= 0 && isDashing == false)
-        {
-            moveSpeed += dashBoost;
-            _dashTime = dashTime;
-            isDashing = true;
-        }
-
-        if (_dashTime <= 0 && isDashing == true)
-        {
-            moveSpeed -= dashBoost;
-            isDashing = false;
-        }
-        else if (isDashing == true)
-        {
-            _dashTime -= Time.deltaTime;
-        }
-        //dash
-
+    private void Update() {
         PlayerInput();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         AdjustPlayerFacingDirection();
         Move();
     }
 
-    private void PlayerInput()
-    {
+    private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
-        myAnimator.SetFloat("moveX",movement.x);
-        myAnimator.SetFloat("moveY",movement.y);
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move()
-    {
+    private void Move() {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection()
-    {
+    private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x)
-        {
-            mySpriteRenderer.flipX = true;
+        if (mousePos.x < playerScreenPoint.x) {
+            mySpriteRender.flipX = true;
             FacingLeft = true;
-        }
-        else
-        {
-            mySpriteRenderer.flipX = false;
+        } else {
+            mySpriteRender.flipX = false;
             FacingLeft = false;
         }
     }
