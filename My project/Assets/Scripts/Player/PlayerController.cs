38e715a74f9ac62284f854a7c8f30ca93848ef16
XuanTrieu4013 <xuantrieu6123@gmail.com>
2025-12-28 01:37:14 +0700

    big update

--- My project/Assets/Scripts/Player/PlayerController.cs
+++ My project/Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : Singleton<PlayerController>
@@ -7,11 +7,10 @@ public class PlayerController : Singleton<PlayerController>
     public bool FacingLeft { get { return facingLeft; } }
     
 
-    [SerializeField] private float moveSpeed = 4f;
-    [SerializeField] private float dashForce = 10f;       // lực dash
-    [SerializeField] private float dashDuration = 0.2f;   // thời gian dash
-    [SerializeField] private float dashCooldown = 0.5f;   // thời gian hồi
-    [SerializeField] private TrailRenderer myTrailRender;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float dashSpeed = 4f;
+    [SerializeField] private TrailRenderer myTrailRenderer;
+    [SerializeField] private Transform weaponCollider;
 
     private PlayerControls playerControls;
     private Vector2 movement;
@@ -22,92 +21,79 @@ public class PlayerController : Singleton<PlayerController>
 
     private bool facingLeft = false;
     private bool isDashing = false;
-    private bool canDash = true;
 
-    protected override void Awake()
-    { 
+    protected override void Awake() {
         base.Awake();
-        
+
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void Start()
-    {
-        playerControls.Combat.Dash.performed += _ => TryDash();
+    private void Start() {
+        playerControls.Combat.Dash.performed += _ => Dash();
+
         startingMoveSpeed = moveSpeed;
     }
 
-    private void OnEnable() => playerControls.Enable();
-    private void OnDisable() => playerControls.Disable();
-
-    private void Update() => PlayerInput();
-
-    private void FixedUpdate()
-    {
-        AdjustPlayerFacingDirection();
-        if (!isDashing) Move();
+    private void OnEnable() {
+        playerControls.Enable();
     }
 
-    private void PlayerInput()
-    {
+    private void Update() {
+        PlayerInput();
+    }
+
+    private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    public Transform GetWeaponCollider() {
+        return weaponCollider;
+    }
+
+    private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move()
-    {
+    private void Move() {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection()
-    {
+    private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x)
-        {
+        if (mousePos.x < playerScreenPoint.x) {
             mySpriteRender.flipX = true;
             facingLeft = true;
-        }
-        else
-        {
+        } else {
             mySpriteRender.flipX = false;
             facingLeft = false;
         }
     }
 
-    private void TryDash()
-    {
-        if (canDash && !isDashing)
-        {
-            StartCoroutine(DashRoutine());
+    private void Dash() {
+        if (!isDashing) {
+            isDashing = true;
+            moveSpeed *= dashSpeed;
+            myTrailRenderer.emitting = true;
+            StartCoroutine(EndDashRoutine());
         }
     }
 
-    private IEnumerator DashRoutine()
-    {
-        isDashing = true;
-        canDash = false;
-
-        if (myTrailRender != null)
-            myTrailRender.emitting = true;
-
-        // Dash theo hướng di chuyển hiện tại
-        Vector2 dashDir = movement.normalized;
-        rb.AddForce(dashDir * dashForce, ForceMode2D.Impulse);
-
-        yield return new WaitForSeconds(dashDuration);
-
-        if (myTrailRender != null)
-            myTrailRender.emitting = false;
-
+    private IEnumerator EndDashRoutine() {
+        float dashTime = .2f;
+        float dashCD = .25f;
+        yield return new WaitForSeconds(dashTime);
+        moveSpeed = startingMoveSpeed;
+        myTrailRenderer.emitting = false;
+        yield return new WaitForSeconds(dashCD);
         isDashing = false;
-
-        yield return new WaitForSeconds(dashCooldown);
-        canDash = true;
     }
 }
--- My project/Assets/Scripts/Player/Sword.cs
+++ My project/Assets/Scripts/Player/Sword.cs
@@ -2,142 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class Sword : MonoBehaviour, IWeapon
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
-    [SerializeField] private Transform weaponCollider;
     [SerializeField] private float swordAttackCD = .5f;
+    [SerializeField] private WeaponInfo weaponInfo;
 
-    private PlayerControls playerControls;
+    private Transform weaponCollider;
     private Animator myAnimator;
-    private PlayerController playerController;
-    private ActiveWeapon activeWeapon;
-    private bool attackButtonDown, isAttacking = false;
+
     private GameObject slashAnim;
 
-    // callback lưu lại để hủy khi disable
-    private System.Action<UnityEngine.InputSystem.InputAction.CallbackContext> attackCallback;
-
-    private void Awake()
-    {
-        playerController = GetComponentInParent<PlayerController>();
-        activeWeapon = GetComponentInParent<ActiveWeapon>();
+    private void Awake() {
         myAnimator = GetComponent<Animator>();
-        playerControls = new PlayerControls();
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-
-        // đăng ký callback
-        attackCallback = ctx => Attack();
-        playerControls.Combat.Attack.started += attackCallback;
     }
 
     private void Start()
     {
-        playerControls.Combat.Attack.started += _ => StartAttacking();
-        playerControls.Combat.Attack.canceled += _ => StopAttacking();
-    }
-    private void OnDisable()
-    {
-        // hủy callback khi object disable/destroy
-        if (attackCallback != null)
-        {
-            playerControls.Combat.Attack.started -= attackCallback;
-        }
-        playerControls.Disable();
+        weaponCollider = PlayerController.Instance.GetWeaponCollider();
+        slashAnimSpawnPoint = GameObject.Find("SlashSpawnPoint").transform;
     }
 
-    private void Update()
-    {
+    private void Update() {
         MouseFollowWithOffset();
-        Attack();
     }
 
-    private void StartAttacking()
+    public WeaponInfo GetWeaponInfo()
     {
-        attackButtonDown = true;
+        return weaponInfo;
     }
 
-    private void StopAttacking()
-    {
-        attackButtonDown = false;
+    public void Attack() {
+
+        myAnimator.SetTrigger("Attack");
+        weaponCollider.gameObject.SetActive(true);
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+
     }
 
-    private void Attack()
-    {
-        if (attackButtonDown && !isAttacking )
-        {
-            isAttacking = true;
-            myAnimator.SetTrigger("Attack");
-            weaponCollider.gameObject.SetActive(true);
-            slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
-            slashAnim.transform.parent = this.transform.parent;
-            StartCoroutine(AttackCDRoutine());
+   
+
+    public void DoneAttackingAnimEvent() {
+        weaponCollider.gameObject.SetActive(false);
+    }
+
+
+    public void SwingUpFlipAnimEvent() {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
+
+        if (PlayerController.Instance.FacingLeft) { 
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
         }
     }
 
-    private IEnumerator AttackCDRoutine()
-    {
-        yield return new WaitForSeconds(swordAttackCD);
-        isAttacking = false;
-    }
+    public void SwingDownFlipAnimEvent() {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
 
-    // Animation Events
-    public void DoneAttackingAnimEvent()
-    {
-        if (weaponCollider != null)
+        if (PlayerController.Instance.FacingLeft)
         {
-            weaponCollider.gameObject.SetActive(false);
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
         }
     }
 
-    public void SwingUpFlipAnimEvent()
-    {
-        if (slashAnim != null)
-        {
-            slashAnim.transform.rotation = Quaternion.Euler(-180, 0, 0);
-
-            if (playerController != null && playerController.FacingLeft)
-            {
-                slashAnim.GetComponent<SpriteRenderer>().flipX = true;
-            }
-        }
-    }
-
-    public void SwingDownFlipAnimEvent()
-    {
-        if (slashAnim != null)
-        {
-            slashAnim.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-            if (playerController != null && playerController.FacingLeft)
-            {
-                slashAnim.GetComponent<SpriteRenderer>().flipX = true;
-            }
-        }
-    }
-
-    private void MouseFollowWithOffset()
-    {
-        if (playerController == null || activeWeapon == null || weaponCollider == null) return;
-
+    private void MouseFollowWithOffset() {
         Vector3 mousePos = Input.mousePosition;
-        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(PlayerController.Instance.transform.position);
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
-        if (mousePos.x < playerScreenPoint.x)
-        {
-            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        if (mousePos.x < playerScreenPoint.x) {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180, angle);
             weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
-        }
-        else
-        {
-            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        } else {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
             weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
