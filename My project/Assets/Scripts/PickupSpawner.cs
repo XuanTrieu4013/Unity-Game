ee2f8d8fdc4303b7f78106c62da1da66d9331b7d
XuanTrieu4013 <xuantrieu6123@gmail.com>
2026-04-06 22:08:05 +0700

    update

--- My project/Assets/Scripts/PickupSpawner.cs
+++ My project/Assets/Scripts/PickupSpawner.cs
@@ -21,7 +21,7 @@ public class PickupSpawner : MonoBehaviour
         }
         if(randomNum == 3)
         {
-            int ramdomAmountOfGold = Random.Range(1, 4);
+            int ramdomAmountOfGold = Random.Range(1, 9);
 
             for(int i = 0; i < ramdomAmountOfGold; i++)
             {
